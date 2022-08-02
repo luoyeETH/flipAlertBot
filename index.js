54ee62f0cd9182ccf37935a991fcecd7fabf111e
luoye <luoyeETH@gmail.com>
2022-08-02 15:59:13 +0800

    新增mint后提醒

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const logWhaleAddress = whaleAddress.map(address => `0x000000000000000000000000$
 
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
 const API_URL = `wss://eth-mainnet.alchemyapi.io/v2/${config.alchemyKey}`
+const API_URL_HTTPS = `https://eth-mainnet.alchemyapi.io/v2/${config.alchemyKey}`
+
 const web3 = createAlchemyWeb3(API_URL)
+const web3_https = createAlchemyWeb3(API_URL_HTTPS)
 const { Webhook } = require('discord-webhook-node');
 const DC_URL = config.discordKey;
 const hook = new Webhook(DC_URL);
@@ -129,12 +132,136 @@ const getSlugFromContract = async (asset_contract) => {
   return slug;
 }
 
+// 通过openseaKey获取版税
+const getTax = async (slug) => {
+  const options = {
+    method: 'GET',
+    url: 'https://api.opensea.io/api/v1/collection/' + slug,
+    headers: {'X-API-KEY': openseaKey}
+  };
+  let response = await axios.request(options).catch(err => {
+    console.log(err);
+  });
+  let jsonData = response.data;
+  let tax = jsonData.collection.primary_asset_contracts[0].seller_fee_basis_points;
+  tax = (tax / 100).toFixed(2);
+  return tax;
+}
+
+// 通过openseaKey获取相关链接
+const getLinks = async (slug) => {
+  const options = {
+    method: 'GET',
+    url: 'https://api.opensea.io/api/v1/collection/' + slug,
+    headers: {'X-API-KEY': openseaKey}
+  };
+  let response = await axios.request(options).catch(err => {
+    console.log(err);
+  });
+  let jsonData = response.data;
+  let discord = jsonData.collection.discord_url;
+  let twitter = jsonData.collection.twitter_username;
+  twitter = twitter ? `https://twitter.com/${twitter}` : '';
+  let website = jsonData.collection.external_url;
+  return {discord, twitter, website};
+}
+
+
+// 获取nft余额
+const getNftBalance = async (contract) => {
+  let nftBalance = 0;
+  let child = fork('./checkNFT.js', [contract]);
+  console.log('fork return pid: ' + child.pid);
+  child.on('message', (msg) => {
+    nftBalance = msg;
+  });
+  child.on('exit', (code) => {
+    console.log(`checkNFT child process ${child.pid} exited with ${code}`);
+  });
+  // 阻塞 等待子进程结束
+  await new Promise((resolve) => {
+    child.on('exit', resolve);
+  }
+  );
+  return nftBalance;
+}
+
+
+// mint后通知
+const mintAlert = async (tx) => {
+  // 计算gas
+  let res = await web3.eth.getTransactionReceipt(tx.hash)
+  let gasUsed = res.gasUsed
+  let gasPrice = parseFloat(web3.utils.fromWei(tx.gasPrice, 'ether'))
+  let Gwei = parseInt(web3.utils.fromWei(tx.gasPrice, 'Gwei'));
+  let gasCost = gasUsed * gasPrice
+  // 获取eth价格
+  const options = {
+    method: 'GET',
+    url: 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD',
+  }
+  let response = await axios.request(options)
+  let ethPrice = response.data.USD
+  let gas = (gasCost * ethPrice).toFixed(2)
+
+  // 是否free mint
+  let freeMint = false;
+  let value = parseFloat(web3.utils.fromWei(tx.value, 'ether'))
+  if (value == 0) {
+    freeMint = true;
+  }
+
+  // 获取os链接
+  let slug = await getSlugFromContract(tx.to);
+  let openSeaUrl = `https://opensea.io/collection/${slug}`
+
+  // etherscan链接
+  let etherscanUrl = `https://etherscan.io/address/${tx.to}/#writeContract`
+
+  // 获取版税  
+  let tax = await getTax(slug);
+  tax = parseFloat(tax);
+
+  // 获取相关链接
+  let links = await getLinks(slug);
+
+  let discord = links.discord;
+  let twitter = links.twitter;
+  let website = links.website;
+  let mintfun = `https://mint.fun/${tx.to}`;
+
+  // 获取单地址nft余额
+  let nfts = await web3_https.alchemy.getNfts({owner: tx.from, contractAddresses: [tx.to]});
+  let nftCount = nfts.totalCount;
+
+  // 授权价格
+  let approveGasUsed = 47000;
+  let approveCost = approveGasUsed * gasPrice;
+  let approvePrice = (approveCost * ethPrice).toFixed(2);
+
+  // 计算NFT回本价
+  let cost = (value + gasCost + approveCost);
+  let flipPrice = ((cost / nftCount) / (1 - tax / 100)).toFixed(4);
+
+  // 获取库存前等待
+  await sleep(30000);
+  let nftBalance = await getNftBalance(tx.to);
+
+  // 推送信息
+  let message = `[mint通知] [免费:${freeMint}]\ngas消耗: ${gasCost.toFixed(5)}ETH ${gas}刀 \nmint时: GasPrice: ${Gwei} eth价格: ${ethPrice}\n版税: ${tax}% 授权预估: ${approvePrice}刀 回本价: ${flipPrice}ETH\n总库存: ${nftBalance} 一号${nftCount}个\n\nOpenSea: ${openSeaUrl}\netherscan: ${etherscanUrl}\nMintfun: ${mintfun}\nDiscord: ${discord}\nTwitter: ${twitter}\nWebsite: ${website}`;
+  console.log(message);
+  await dc(message);
+  await bark("mint", message);
+  await ding(message);
+
+}
+
+
 // 初始化os监控程序 适用于断线重连
 const initApp = async () => {
   let slugList = await readFromFile('slug.txt');
   if (slugList.length === 0) {
     console.log("slug数组为空");
-    return;
   } else {
     for (let i = 0; i < slugList.length; i++) {
       let date = await getDate();
@@ -202,6 +329,7 @@ const setNftContract = (tx) => {
   if(contractHistory.includes(tx.to) === false) {
     contractHistory.push(tx.to);
     startApp(tx.to);
+    mintAlert(tx);
   }
 }
 const doSomethingWithTxn = (txn) => {
